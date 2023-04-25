5594b3cb141cf01235841ae5923ac02d6fb7331f
jametl <jametl@ttu.ee>
2023-04-25 16:59:47 +0300

    initial empty repo

---
+++ src/main/java/com/example/Jaan/Wisercat/WisercatApplication.java
@@ -0,0 +1,13 @@
+package com.example.Jaan.Wisercat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WisercatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WisercatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Jaan/Wisercat/WisercatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Jaan.Wisercat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WisercatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
